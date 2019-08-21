a52d59b0cd2a5c9a750bfc93bc42235ca0c342ed
Tim Sherman <tim.rs.sherman@gmail.com>
2019-08-20 22:38:31 -0700

    new survey posting to friendsData, needing to compare and find match, once found create modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,9 @@ module.exports = function(app) {
     app.get("/api/survey", function(req, res) {
         res.json(friendsData);
       });
+
+    app.post("/api/survey", function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    })
 };
