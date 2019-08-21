76856a1a302a372fda3af7853f99ffca63d4e3fc
Tim Sherman <tim.rs.sherman@gmail.com>
2019-08-21 15:04:37 -0700

    complete, needing readME

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,38 @@
 //Load Data
 var friendsData = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/survey", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/survey", function (req, res) {
         res.json(friendsData);
-      });
+    });
 
     app.post("/api/survey", function (req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+        var newFriend = req.body;
+        var newScore = newFriend.scores.reduce(
+            (accum, value) => parseInt(accum) + parseInt(value));
+
+        var bestMatch = {
+            index: 0,
+            score: friendsData[0].scores.reduce((accum, value) => parseInt(accum) + parseInt(value))
+        }
+
+        for (let i = 1; i < friendsData; i++) {
+            var value = friendsData[i].scores.reduce(
+                (accum, value) => parseInt(accum) + parseInt(value)
+            );
+            var currentDiff = Math.abs(newScore - value);
+            var bestDiff = Math.abs(newScore - bestMatch.score);
+            
+            if (currentDiff < bestDiff) {
+                bestMatch = {
+                    index: i,
+                    score: value
+                }
+            }
+        }
+
+
+        friendsData.push(newFriend);
+        res.json(friendsData[bestMatch.index]);
     })
 };
